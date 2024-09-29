dc4089d2244615dd17376b49357a3874f6b521a5
Ariel Tequida <ariel.tequida@gmail.com>
2024-09-28 23:00:26 -0700

    MostrarPalabras e IngresarPalabras anhadidos

--- prueba-palabras.py
+++ prueba-palabras.py
@@ -1,17 +1,53 @@
+import random
+
 palabrasConocidas = {"clave": "valor"}
-palabrasPracticar = {"would": "lo haria", "witness": "testigo", "whistle": "silvido"}
+palabrasPracticar = {"Would": "lo haria", "Witness": "testigo", "Whistle": "silvido"}
 palabrasAprendidas = {"clave": "valor"}
 
 def mostrarPalabras():
-    pass
+    # Extraemos las llaves
+    listallaves = list(palabrasPracticar)
+    
+    # Randomizamos las opciones
+    primerNum = random.randrange(len(listallaves))
+    segundoNum = random.randrange(len(listallaves))
+    tercerNum = random.randrange(len(listallaves))
+    
+    # Verificamos que no se repitan
+    while segundoNum == primerNum:
+        segundoNum = random.randrange(len(listallaves))
+    while tercerNum == primerNum or tercerNum == segundoNum:
+        tercerNum = random.randrange(len(listallaves))
+
+    # Mostramos
+    print(" -"+ listallaves[primerNum] +": "+ palabrasPracticar[listallaves[primerNum]])
+    print(" -"+ listallaves[segundoNum] +": "+ palabrasPracticar[listallaves[segundoNum]])
+    print(" -"+ listallaves[tercerNum] +": "+ palabrasPracticar[listallaves[tercerNum]])
+
 
 def ingresarPalabras():
-    pass
+    nuevaPalabra = input("Introduce una nueva palabra: ").capitalize()
+
+    # Verificamos que no se repitan
+    listallaves = list(palabrasPracticar)
+    if nuevaPalabra in listallaves:
+        return print("Palabra ya en el diccionario")
+    
+    nuevaTraduccion = input("Introduce su traduccion: ").capitalize()
+
+    # Ingresamos la palabra al diccionario
+    palabrasPracticar[nuevaPalabra] = nuevaTraduccion
+    print("Palabra registrada!")
 
 if __name__ == "__main__":
-    print("Que deseas hacer?\n1) Practicar tres palabras\n2) Ingresar una palabra nueva")
-    if input("Introduce una opcion: ") == "1" or "2":
-        print("ok")
-    else: 
-        print("reintentalo")
+    while True:
+        print("** Que deseas hacer? **\n1) Practicar tres palabras\n2) Ingresar una palabra nueva")
+        opcion = input("Introduce una opcion: ")
+        if opcion == "1" or opcion == "2":
+            if opcion == "1":
+                mostrarPalabras()
+            else:
+                ingresarPalabras()
+        else: 
+            print("Opcion no valida")
 
